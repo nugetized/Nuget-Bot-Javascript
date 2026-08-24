7d78c5e9af237ec34b9b52d34cf4badae486325d
Dennis <dijkzond@gmail.com>
2026-08-24 19:36:58 +0200

    meow

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('
 for (const file of commandFiles) {
     const filePath = path.join(commandsPath, file)
     const command = require(filePath)
-
+    
     if ('data' in command && 'execute' in command) {
         client.commands.set(command.data.name, command)
     }
